11c940df6148687d16eef45c763fc1064353075a
Yevhenii Alieksandrov <evhenii.aleksandrov@otakoyi.com>
2023-06-26 17:13:27 +0300

    feat/basic-dev-setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+'use client';
+
+import { signIn } from 'next-auth/react';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1>Hello Next 13</h1>
+      <button
+        type='button'
+        style={{ marginRight: 10 }}
+        onClick={() => signIn()}
+      >
+        Sign in
+      </button>
     </main>
-  )
+  );
 }
