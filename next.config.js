57cfaa85b70e2847c92b711de2aa3b8cc995a85b
Yevhenii Alieksandrov <evhenii.aleksandrov@otakoyi.com>
2023-12-26 22:15:49 +0200

    fix: config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    STRIPE_PUBLISH_KEY: process.env.STRIPE_PUBLISH_KEY,
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-  },
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
