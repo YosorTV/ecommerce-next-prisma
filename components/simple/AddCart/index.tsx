af04c647a738b820dc10ffbee207b913c35568b9
Yevhenii Alieksandrov <evhenii.aleksandrov@otakoyi.com>
2023-12-26 23:58:18 +0200

    fix test

--- components/simple/AddCart/index.tsx
+++ components/simple/AddCart/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FC, useState } from 'react';
+import { FC } from 'react';
 
 import { useCart } from '@/store';
 import { AddCartProps } from '@/types';
@@ -10,22 +10,18 @@ import { Button } from '@/components/elements';
 export const AddCart: FC<AddCartProps> = ({ data }) => {
   const cartStore = useCart();
 
-  const [added, setAdded] = useState(false);
+  // const [added, setAdded] = useState(false);
+  // setTimeout(() => {
+  //   setAdded(false);
+  // }, 500);
 
-  const handleAdd = () => {
-    cartStore.onAdd(data);
-    setAdded(true);
-    setTimeout(() => {
-      setAdded(false);
-    }, 500);
-  };
+  const handleAdd = () => cartStore.onAdd(data);
 
   return (
     <Button
       onClick={handleAdd}
       variant='basic'
       className='btn my-3 w-full rounded-none font-medium text-white md:w-min'
-      disabled={added}
     >
       Add to cart
     </Button>
